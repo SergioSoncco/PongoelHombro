ec796396554c653f9764fb1a9bdf374d5795b36d
Emilio Valdivia <evaldiviaci@unsa.edu.pe>
2021-11-18 20:13:14 -0500

    recyclerhome

--- app/src/main/java/com/lab02/pongoelhombro/View/HomeFragment.java
+++ app/src/main/java/com/lab02/pongoelhombro/View/HomeFragment.java
@@ -1,16 +1,32 @@
 package com.lab02.pongoelhombro.View;
 
+import static com.lab02.pongoelhombro.R.drawable.unsacovid;
+
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.lab02.pongoelhombro.Model.Noticia;
+import com.lab02.pongoelhombro.Presenter.myadapter;
 import com.lab02.pongoelhombro.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +42,12 @@ public class HomeFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
      Button primera_dosis;
      View  vista;
+     FirebaseFirestore db= FirebaseFirestore.getInstance();
+     RecyclerView lista;
+     TextView prueba;
+    private ArrayList<Noticia> news;
+
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -69,16 +91,33 @@ public class HomeFragment extends Fragment {
         
         vista=inflater.inflate(R.layout.fragment_home, container, false);
 
-        primera_dosis=vista.findViewById(R.id.primera_dosis);
-        primera_dosis.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                Intent intent=new Intent(vista.getContext(),MainActivity2.class);
-                intent.putExtra("fragment",1);
-                startActivity(intent);
-            }
-        });
+        prueba=vista.findViewById(R.id.notmin);
+        lista=vista.findViewById(R.id.recyview);
+        news=new ArrayList<Noticia>();
+        db.collection("Noticia")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult())
+                            {
+                                Noticia noticia=new Noticia(unsacovid,document.get("NotTit" )+"",document.get("NotDes")+"");
+                                news.add(noticia);
+                                prueba.setText("Noticias actuales");
+
+                                Log.d("TAG", document.getId() + " => " + document.getData());
+                            }
+
+                        } else {
+                            Log.w("TAG", "Error getting documents.", task.getException());
+
+                        }
+                    }
+                });
+        lista.setLayoutManager(new LinearLayoutManager(getContext()));
+        lista.setAdapter(new myadapter(news));
         return  vista;
     }
 }
